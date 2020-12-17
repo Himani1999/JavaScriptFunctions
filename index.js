5ee48e7e236e2d5cca25a26da8d26b8f0ccd9e93
Himani Sharma <sharmahimani565@gmail.com>
2020-12-17 16:43:03 +0530

    new

---
+++ index.js
@@ -0,0 +1,28 @@
+/* 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'));
+
+*/
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+
+function myFunc() {
+    document.getElementById("click").innerHTML = "Hello World";
+}
+
+
+function abc() {
+    var x = 2;
+    var y = 8;
+    var z = x + y;
+    document.getElementById("z").innerHTML = x + y;
+}
